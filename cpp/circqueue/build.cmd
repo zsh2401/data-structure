945b1869e535cf9e0834c623cb5abfd3641e593a
zsh2401 <zsh2401@163.com>
2020-03-06 17:42:44 +0800

    更新队列代码

---
+++ cpp/circqueue/build.cmd
@@ -0,0 +1,2 @@
+@ECHO OFF
+g++ circqueue.cpp test.cpp -o test.exe
--- cpp/circqueue/circqueue.cpp
+++ cpp/circqueue/circqueue.cpp
@@ -1,26 +1,23 @@
-#include "../fx.h"
-using namespace fx;
-#define __CQUEUE_ELE_CAPCITY 6
-#define MOD %
-#define __CQUEUE_ARR_SIZE (__CQUEUE_ELE_CAPCITY + 1)
-typedef struct queue
+#include "circqueue.h"
+#include <stdlib.h>
+using namespace cqueue;
+namespace cqueue
 {
-    size_t front;
-    size_t rear;
-    data_t *array;
-} * CQueue;
-
 ExecStatus CQueue_init(CQueue &q)
 {
     q = (CQueue)malloc(sizeof(CQueue));
-    q->array = (data_t *)malloc(sizeof(data_t) * __CQUEUE_ARR_SIZE);
-    q->front = 0;
-    q->rear = 0;
+    q->array = (data_t *)malloc(sizeof(data_t) * __CQUEUE_SPACE_SIZE);
+    q->front = q->rear = 0;
     return RETCODE_SUCCESS;
 }
+size_t CQueue_sizeof(CQueue q)
+{
+    return (q->rear - q->front + __CQUEUE_SPACE_SIZE) % __CQUEUE_SPACE_SIZE;
+}
+
 ExecStatus CQueue_full(CQueue q)
 {
-    return (q->rear + 1) % __CQUEUE_ARR_SIZE == q->front;
+    return (q->rear + 1) % __CQUEUE_SPACE_SIZE == q->front;
 }
 bool CQueue_empty(CQueue q)
 {
@@ -32,16 +29,16 @@ ExecStatus CQueue_en(CQueue q, data_t data)
     {
         return RETCODE_OVERFLOW;
     }
-    size_t index = (q->rear % __CQUEUE_ARR_SIZE);
+    size_t index = (q->rear % __CQUEUE_SPACE_SIZE);
     q->array[index] = data;
-    q->rear = (q->rear + 1) % __CQUEUE_ARR_SIZE;
+    q->rear = (q->rear + 1) % __CQUEUE_SPACE_SIZE;
     return RETCODE_SUCCESS;
 }
 
 ExecStatus CQueue_de(CQueue q, data_t &data)
 {
     data = q->array[q->front];
-    q->front = (q->front + 1 % __CQUEUE_ARR_SIZE);
+    q->front = (q->front + 1 % __CQUEUE_SPACE_SIZE);
     return RETCODE_SUCCESS;
 }
 ExecStatus CQueue_clear(CQueue q)
@@ -61,3 +58,4 @@ ExecStatus CQueue_destory(CQueue &q)
     q = NULL;
     return RETCODE_SUCCESS;
 }
+} // namespace cqueue
